5f31fe0dd4924756c35eb33fe04161da2252e47e
jofredogivan <jofredogivan@gmail.com>
2025-06-13 20:49:27 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,7 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('keepecar6-cache-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './service-worker.js'
-      ]);
-    })
-  );
+self.addEventListener('install', function(e) {
+  console.log('Service Worker: Instalado');
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request).catch(() => new Response('Sem conexão')));
 });
